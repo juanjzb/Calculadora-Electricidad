604d7e1b6ecdfb9d339cb6fa26c9a3a3ef00e98c
LyricalString <79110588+LyricalString@users.noreply.github.com>
2021-03-09 21:10:07 +0100

    Add files via upload

--- Web/functions.js
+++ Web/functions.js
@@ -9,60 +9,38 @@ magnitudes={
 function check() {
   console.log('Checkeado');
 }
-// exacto asi,
+
 function insert() {
-  clave = document.getElementById("inputMagnitud").value;
-  valor = document.getElementById("inputValue").value;
-  if (clave == "Voltaje") {
-  magnitudes.vol[0] = valor;
-  }
-  console.log(magnitudes.vol[0])
+  // clave = document.getElementById("inputMagnitud").value;
+  // valor = document.getElementById("inputValue").value;
+  // if (clave == "Voltaje") {
+  // magnitudes.vol[0] = valor;
+  // }
+  // console.log(magnitudes.vol[0])
   agregarFila();
 }
 
 function calc() {
-  if (!isNaN(document.getElementById('inputVol').value)) {
-    var voltaje = parseFloat(document.getElementById('inputVol').value);
-    magnitudes.vol[0] = voltaje;
+
+
+  //var voltaje = parseFloat(document.getElementById('Voltaje').innerHTML);
+  var intensidad = parseFloat(document.getElementById('Intensidad').innerHTML);
+  var resistencia = parseFloat(document.getElementById('Resistencia').innerHTML);
+  var potencia = parseFloat(document.getElementById('Potencia').innerHTML);
+
+  if (!isNaN(parseFloat(document.getElementById('Voltaje').innerHTML)) || parseFloat(document.getElementById('Voltaje').innerHTML!="")) {
+     voltaje = parseFloat(document.getElementById('Voltaje').innerHTML;
+     console.log(voltaje);
+    //magnitudes.vol[0] = voltaje; //esto ya no lo ocupas, por la logica que lleva creo
   }
   else{
-    var voltaje = 0;
-  }
-  if (!isNaN(document.getElementById('inputInt').value)) {
-    var intensidad = parseFloat(document.getElementById('inputInt').value);
-    magnitudes.int[0] = intensidad;
-  }
-  else{
-    var intensidad = 0;
-  }
-  if (!isNaN(document.getElementById('inputRes').value)) {
-    var resistencia = parseFloat(document.getElementById('inputRes').value);
-    magnitudes.res[0] = resistencia;
-  }
-  else{
-    var resistencia = 0;
-  }
-  if (!isNaN(document.getElementById('inputPot').value)) {
-    var potencia = parseFloat(document.getElementById('inputPot').value);
-    magnitudes.pot[0] = potencia;
-  }
-  else{
-    var potencia = 0;
+     voltaje = 0;
   }
 
   calcVol(intensidad, resistencia, potencia);
   calcInt(voltaje, resistencia, potencia);
   calcRes(intensidad, voltaje, potencia);
   calcPot(intensidad, voltaje, resistencia);
-
-  document.getElementById('inputVol').value = '';
-  document.getElementById('inputInt').value = '';
-  document.getElementById('inputRes').value = '';
-  document.getElementById('inputPot').value = '';
-  document.getElementById('finalInt').innerHTML = 'Intensidad: ' + magnitudes.int[1] + 'A';
-  document.getElementById('finalVol').innerHTML = 'Voltaje: ' + magnitudes.vol[1] + 'V';
-  document.getElementById('finalRes').innerHTML = 'Resistencia: ' + magnitudes.res[1] + 'Ω';
-  document.getElementById('finalPot').innerHTML = 'Potencia: ' + magnitudes.pot[1] + 'W';
 }
 
 function calcInt(voltaje, resistencia, potencia) {
@@ -82,6 +60,7 @@ function calcVol(intensidad, resistencia, potencia) {
   if (intensidad && resistencia) {
     result = intensidad * resistencia;
     magnitudes.vol[1] = result;
+    document.write(magnitudes.vol[1]);
   } else if (potencia && intensidad) {
     result = potencia / intensidad;
     magnitudes.vol[1] = result;
@@ -115,15 +94,4 @@ function calcPot(intensidad, voltaje, resistencia) {
     result = (voltaje ** 2) / resistencia;
     magnitudes.pot[1] = result;
   }
-}
-
-function reseteo() {
-  magnitudes.vol[1] = 0;
-  magnitudes.int[1] = 0;
-  magnitudes.res[1] = 0;
-  magnitudes.pot[1] = 0;
-  document.getElementById('finalVol').innerHTML = 'Voltaje: 0V';
-  document.getElementById('finalInt').innerHTML = 'Intensidad: 0A';
-  document.getElementById('finalRes').innerHTML = 'Resistencia: 0Ω';
-  document.getElementById('finalPot').innerHTML = 'Potencia: 0W';
 }
--- Web/table.js
+++ Web/table.js
@@ -1,13 +1,21 @@
 function agregarFila(){
   magnitud = document.getElementById("inputMagnitud").value;
   valor = document.getElementById("inputValue").value;
-  document.getElementById("tabla").insertRow(-1).innerHTML = '<td>'+ magnitud + '</td><td>'+ valor + '</td><td><button class="boton"></td>';
+  document.getElementById("tabla").insertRow(-1).innerHTML = '<td>'+ magnitud + '</td><td id="'+ magnitud +'">'+ valor + '</td><td><button onClick="eliminarFila()" class="boton">X</td>';
 }
 
 function eliminarFila(){
-  var table = document.getElementById("tabla");
-  var rowCount = table.rows.length;
+  var td = event.target.parentNode;
+      var tr = td.parentNode; // the row to be removed
+      tr.parentNode.removeChild(tr);
+      magnitudes.vol[0] = 0;
+}
 
-  if(rowCount > 1)
-    table.deleteRow(rowCount -1);
+function calcular(){
+
+var valIntensidad = parseInt(document.getElementById('Intensidad').innerHTML);
+var valResistencia = parseInt(document.getElementById('Resistencia').innerHTML);
+document.write( "Voltaje= " + (valIntensidad * valResistencia));
+//
+//Ahora si!, hay que validar todo lo de antes, segun el valor que se inserto y eso
 }
